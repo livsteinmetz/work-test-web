ba9c499bd6017ff0bee16fa661f8f0ca4fd4661b
livsteinmetz <olivia.r.steinmetz@gmail.com>
2025-11-29 13:26:31 -0500

    create layout

---
+++ src/components/Layout.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import type { ReactNode } from "react";
+
+interface Props {
+  children: ReactNode;
+}
+
+export function Layout({ children }: Props) {
+  return (
+    <div className="relative min-h-screen bg-offwhite flex">
+
+      <div className="fixed top-6 left-6 z-50">
+        <img
+          src="/images/munchies-logo.png"
+          alt="Munchies"
+          className="h-10 w-auto"
+        />
+      </div>
+
+      <div className="flex w-full gap-10 px-[40px] pt-[144px] pb-16">
+        {children}
+      </div>
+    </div>
+  );
+}
